d9960bbe592a656fc8c99c52127f1ac2d40ca76b
Nicolas De Jesus <ndejesus34800@gmail.com>
2021-10-05 12:02:34 +0200

    add some function and test

---
+++ calendar/Calendar.php
@@ -0,0 +1,82 @@
+<?php
+
+class Calendar
+{
+
+    public function __construct($year, $months)
+    {
+        $this->showDays($year, $months);
+    }
+
+    public function addEvent()
+    {
+
+    }
+
+    public function deleteEvent()
+    {
+
+    }
+
+    private function showDays($year, $months)
+    {
+        echo $this->showMonths($months) . " " . $this->showYears($year) . "</br>";
+        $daysNumber = cal_days_in_month(CAL_GREGORIAN, $months, $year);
+        $i = 0;
+        while($i < $daysNumber)
+        {
+            echo "<p style=\" display: inline-block; margin-left: 20px; \">" . $i + 1 . "</p>";
+            $i++;
+        }
+    }
+
+    private function showMonths($months)
+    {
+        switch ($months) {
+            case '1':
+                return "Janvier";
+                break;
+            case '2':
+                return "Février";
+                break;
+            case '3':
+                return "Mars";
+                break;
+            case '4':
+                return "Avril";
+                break;
+            case '5':
+                return "Mai";
+                break;
+            case '6':
+                return "Juin";
+                break;
+            case '7':
+                return "Juillet";
+                break;
+            case '8':
+                return "Août";
+                break;
+            case '9':
+                return "Septembre";
+                break;
+            case '10':
+                return "Octobre";
+                break;
+            case '11':
+                return "Novembre";
+                break;
+            case '12':
+                return "Décembre";
+                break;
+            default:
+                return "Erreur";
+                break;
+        }
+    }
+
+    private function showYears($year)
+    {
+        return $year;
+    }
+}

9a0171f222b60bbd4ade0949f0775f8ccffebbdb
Nicolas Paillard <paillard.nicolas.pro@gmail.com>
2021-10-05 14:31:25 +0200

    update homepage

--- index.php
+++ index.php
@@ -1,29 +1,36 @@
+<?php
+	include('temp_server.php');
+?>
 <html>
 <head>
     <title>Accueil</title>
-    <link rel="stylesheet" href="Styles/main.css">
+    <link rel="stylesheet" href="styles/main.css">
 </head>
 
 <body>
 <div class=container>
-<fieldset>
-  <form action="login" method="post">
-    <h1>Connexion</h1>
-    <label for="username">Identifiant :</label>
-    <br/>
-    <input type="text" id="username" name="username" required size="30">
-    <br/>
-    <label for="password">Mot de passe :</label>
-    <br/>
-    <input type="password" id="password" name="password" required size="30">
-    <br/>
-    <br/>
-    <input type="submit" id="login" value="Se connecter">
-  </form>
   <?php
-	include('temp_server.php');
-?>
-</fieldset>
+if(isset($user_name)){
+	echo $user_name;
+}else{
+  echo'
+  <fieldset>
+    <form action="login" method="post">
+      <h1>Connexion</h1>
+      <label for="username">Identifiant :</label>
+      <br/>
+      <input type="text" id="username" name="username" required size="30">
+      <br/>
+      <label for="password">Mot de passe :</label>
+      <br/>
+      <input type="password" id="password" name="password" required size="30">
+      <br/>
+      <br/>
+      <input type="submit" id="login" value="Se connecter">
+    </form>
+  </fieldset>'
+}
+  ?>
 
 </div>
 

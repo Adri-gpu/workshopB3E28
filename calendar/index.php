256d0837e4dc0a8c3a8f649c4a930ca0a990271a
Nicolas De Jesus <ndejesus34800@gmail.com>
2021-10-05 12:21:16 +0200

    add html structure for test

--- calendar/index.php
+++ calendar/index.php
@@ -1,5 +1,22 @@
 <?php
+include("Calendar.php");
+?>
 
-include('Calendar.php');
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Calendar</title>
+  <link rel="stylesheet" href="calendar.css">
+  <script src="script.js"></script>
+</head>
+<body>
 
-$calendar = new Calendar(2021, 2);
+<?php
+
+$calendar = new Calendar(2021, 1);
+
+?>
+
+</body>
+</html>

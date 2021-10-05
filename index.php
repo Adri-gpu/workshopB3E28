bfef97bfaa80c1a95f96ae48073c8ca79afd8fd0
Nicolas Paillard <paillard.nicolas.pro@gmail.com>
2021-10-05 09:21:06 +0200

    update

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Event Calendar</title>
+    <link rel="stylesheet" href="Styles/main.css">
+</head>
+
+<body>
+<h1>This Month's Events</h1>
+<?php
+  echo ("The calendar goes here.");
+?>
+</body>
+</html>

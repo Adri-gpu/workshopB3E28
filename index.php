d5bfa95f9cce15dbe2b3ab8014c711be650fb6eb
mingfang niclot <mingfang0318@hotmail.com>
2021-10-06 15:06:10 +0200

    implement Api Pole Emploi sur site

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<html>
+    <head>
+        <title>WorkshopB3E28</title>
+    </head>
+    <body>
+        <!-- Module de recherche d'emploi Handicap -->
+        <div style=" border: 2px solid #e66465; width:60%;">
+            <form method="POST" action="/module_recherche/poleEmploiRecherche.php" style="margin: 15px; line-height: 1.5; text-align: center;">
+                <p >Recherche les offres dont les entreprise sont adaptés aux handicapés et "handi friendly"  : </p>
+                <input type="search" name="motCles" placeholder="Recherche par un mot clé..." />
+                <input type="submit" value="Valider" />
+            </form>
+        </div>
+    </body>
+</html>

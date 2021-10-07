6ad257f6f0f60d3a71c134be26a82a04056e18bb
Nicolas De Jesus <ndejesus34800@gmail.com>
2021-10-07 09:52:16 +0200

    fix

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <body>
         <!-- Module de recherche d'emploi Handicap -->
         <div style=" border: 2px solid #e66465; width:60%;">
-            <form method="POST" action="/module_recherche/poleEmploiRecherche.php" style="margin: 15px; line-height: 1.5; text-align: center;">
+            <form method="POST" action="/WORKSHOPB3E28/module_recherche/poleEmploiRecherche.php" style="margin: 15px; line-height: 1.5; text-align: center;">
                 <p >Recherche les offres dont les entreprise sont adaptés aux handicapés et "handi friendly"  : </p>
                 <input type="search" name="motCles" placeholder="Recherche par un mot clé..." />
                 <input type="submit" value="Valider" />

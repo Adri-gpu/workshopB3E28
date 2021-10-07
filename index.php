58b2701f65840d8c15a1f6233e5d6ac52216667c
Nicolas Paillard <paillard.nicolas.pro@gmail.com>
2021-10-07 11:07:28 +0200

    update

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         </div>
         <br/>
         <!-- Module de recherche d'emploi Handicap -->
-        <div style="float:left; border: 2px solid #e66465; width:60%;">
+        <div style="float:left;     border: 2px solid #e66465; width:60%;">
             <form method="POST" action=" /module_recherche/poleEmploiRecherche.php" style="margin: 15px; line-height: 1.5; text-align: center;">
                 <p >Recherche les offres dont les entreprise sont adaptés aux handicapés et "handi friendly"  : </p>
                 <input type="search" name="motCles" placeholder="Recherche par un mot clé..." />

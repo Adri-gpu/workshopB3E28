7134d9878880143291992da5c6d980190268a7aa
Nicolas De Jesus <ndejesus34800@gmail.com>
2021-10-07 10:59:20 +0200

    add interface to select origin and destination

---
+++ directions/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Direction</title>
+</head>
+<body>
+    <form action="directions.php" method="post">
+        <label for="origin">Départ : </label><input type="text" name="origin" required>
+        <br>
+        <br>
+        <label for="destinantion">Arrivé : </label><input type="text" name="destination" required>
+        <br>
+        <br>
+        <button>Recherche</button>
+    </form>
+</body>
+</html>

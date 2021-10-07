acaf11f0167862fe7a92bc815ef630bd0b5f4dff
Nicolas Paillard <paillard.nicolas.pro@gmail.com>
2021-10-07 10:17:30 +0200

    update index (start tiles)

--- index.php
+++ index.php
@@ -2,11 +2,18 @@
 <html>
     <head>
         <title>WorkshopB3E28</title>
+        <link rel="stylesheet" href="index.css">
     </head>
     <body>
+        <div class=container>
+            <div class=tile>a</div>
+            <div class=tile>a</div>
+            <div class=tile>a</div>
+            <div class=tile>a</div>
+        </div>
         <!-- Module de recherche d'emploi Handicap -->
-        <div style=" border: 2px solid #e66465; width:60%;">
-            <form method="POST" action="/WORKSHOPB3E28/module_recherche/poleEmploiRecherche.php" style="margin: 15px; line-height: 1.5; text-align: center;">
+        <div style="float:left; border: 2px solid #e66465; width:60%;">
+            <form method="POST" action=" /module_recherche/poleEmploiRecherche.php" style="margin: 15px; line-height: 1.5; text-align: center;">
                 <p >Recherche les offres dont les entreprise sont adaptés aux handicapés et "handi friendly"  : </p>
                 <input type="search" name="motCles" placeholder="Recherche par un mot clé..." />
                 <input type="submit" value="Valider" />

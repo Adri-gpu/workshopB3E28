1a447287bb3dab99210ed3efbfc7c9bd24814c47
Nicolas Paillard <paillard.nicolas.pro@gmail.com>
2021-10-07 11:07:51 +0200

    aaaaaaaaaaa

--- register/index.php
+++ register/index.php
@@ -1,14 +1,16 @@
 <?php
 $bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
-if(isset($_POST['forminscription'])) {  
-   $pseudo = htmlspecialchars($_POST['pseudo']);
+ 
+if(isset($_POST['forminscription'])) {
+   $nom = htmlspecialchars($_POST['nom']);
+   $prenom = htmlspecialchars($_POST['prenom']);
    $mail = htmlspecialchars($_POST['mail']);
    $mail2 = htmlspecialchars($_POST['mail2']);
    $mdp = sha1($_POST['mdp']);
    $mdp2 = sha1($_POST['mdp2']);
-   if(!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'])) {
-      $pseudolength = strlen($pseudo);
-      if($pseudolength <= 255) {
+   if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'])) {
+      $nomlength = strlen($nom);
+      if($nomlength <= 255) {
          if($mail == $mail2) {
             if(filter_var($mail, FILTER_VALIDATE_EMAIL)) {
                $reqmail = $bdd->prepare("SELECT * FROM membres WHERE mail = ?");
@@ -16,8 +18,8 @@ if(isset($_POST['forminscription'])) {
                $mailexist = $reqmail->rowCount();
                if($mailexist == 0) {
                   if($mdp == $mdp2) {
-                     $insertmbr = $bdd->prepare("INSERT INTO membres(pseudo, mail, motdepasse) VALUES(?, ?, ?)");
-                     $insertmbr->execute(array($pseudo, $mail, $mdp));
+                     $insertmbr = $bdd->prepare("INSERT INTO membres(nom, prenom, mail, motdepasse) VALUES(?, ?, ?, ?)");
+                     $insertmbr->execute(array($nom, $prenom, $mail, $mdp));
                      $erreur = "Votre compte a bien été créé ! <a href=\"connexion.php\">Me connecter</a>";
                   } else {
                      $erreur = "Vos mots de passes ne correspondent pas !";
@@ -32,7 +34,7 @@ if(isset($_POST['forminscription'])) {
             $erreur = "Vos adresses mail ne correspondent pas !";
          }
       } else {
-         $erreur = "Votre pseudo ne doit pas dépasser 255 caractères !";
+         $erreur = "Votre nom ne doit pas dépasser 255 caractères !";
       }
    } else {
       $erreur = "Tous les champs doivent être complétés !";
@@ -54,7 +56,10 @@ if(isset($_POST['forminscription'])) {
                 <h1>Inscription</h1>
                 
                 <label><b>Nom</b></label>
-                <input type="text" id="pseudo" placeholder="Entrer le nom" name="pseudo" required>
+                <input type="text" id="nom" placeholder="Entrer le nom" name="nom" required>
+
+                <label><b>prenom</b></label>
+                <input type="text" id="prenom" placeholder="Entrer le prenom" name="prenom" required>
 
                 <label><b>Adresse Email</b></label>
                 <input type="text" id="mail2" placeholder="Entrer l'adresse email" name="mail" required>
@@ -62,13 +67,14 @@ if(isset($_POST['forminscription'])) {
                 <label><b>Confirmation Adresse Email</b></label>
                 <input type="text" id="mail2" placeholder="Entrer l'adresse email" name="mail2" required>
 
+
                 <label><b>Mot de passe</b></label>
                 <input type="password" id="mdp" placeholder="Entrer le mot de passe" name="mdp" required>
 
                 <label><b>Confirmation Mot de passe</b></label>
                 <input type="password" id="mdp2" laceholder="Entrer le mot de passe" name="mdp2" required>
 
-                <input type="submit" name="forminscription" value='LOGIN'>
+                <input type="submit" name="forminscription" value='LOGIN' >
                 <?php
                 if(isset($_GET['erreur'])){
                     $err = $_GET['erreur'];

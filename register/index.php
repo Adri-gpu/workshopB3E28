b8b5ce233a3e25b8e0ca1952d631f82f23d09d5e
Nicolas Paillard <paillard.nicolas.pro@gmail.com>
2021-10-07 10:33:38 +0200

    update register

--- register/index.php
+++ register/index.php
@@ -1,3 +1,46 @@
+<?php
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
+ 
+if(isset($_POST['forminscription'])) {
+   $pseudo = htmlspecialchars($_POST['pseudo']);
+   $mail = htmlspecialchars($_POST['mail']);
+   $mail2 = htmlspecialchars($_POST['mail2']);
+   $mdp = sha1($_POST['mdp']);
+   $mdp2 = sha1($_POST['mdp2']);
+   if(!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'])) {
+      $pseudolength = strlen($pseudo);
+      if($pseudolength <= 255) {
+         if($mail == $mail2) {
+            if(filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+               $reqmail = $bdd->prepare("SELECT * FROM membres WHERE mail = ?");
+               $reqmail->execute(array($mail));
+               $mailexist = $reqmail->rowCount();
+               if($mailexist == 0) {
+                  if($mdp == $mdp2) {
+                     $insertmbr = $bdd->prepare("INSERT INTO membres(pseudo, mail, motdepasse) VALUES(?, ?, ?)");
+                     $insertmbr->execute(array($pseudo, $mail, $mdp));
+                     $erreur = "Votre compte a bien été créé ! <a href=\"connexion.php\">Me connecter</a>";
+                  } else {
+                     $erreur = "Vos mots de passes ne correspondent pas !";
+                  }
+               } else {
+                  $erreur = "Adresse mail déjà utilisée !";
+               }
+            } else {
+               $erreur = "Votre adresse mail n'est pas valide !";
+            }
+         } else {
+            $erreur = "Vos adresses mail ne correspondent pas !";
+         }
+      } else {
+         $erreur = "Votre pseudo ne doit pas dépasser 255 caractères !";
+      }
+   } else {
+      $erreur = "Tous les champs doivent être complétés !";
+   }
+}
+?>
+
 <html>
     <head>
        <meta charset="utf-8">
@@ -12,22 +55,20 @@
                 <h1>Inscription</h1>
                 
                 <label><b>Nom</b></label>
-                <input type="text" id="nom" placeholder="Entrer le nom" name="username" required>
-
-                <label><b>Prénom</b></label>
-                <input type="text" id="prenom" placeholder="Entrer le prénom" name="username" required>
+                <input type="text" id="pseudo" placeholder="Entrer le nom" name="pseudo" required>
 
                 <label><b>Adresse Email</b></label>
-                <input type="text" id="mail" placeholder="Entrer l'adresse email" name="text" required>
+                <input type="password" id="mail2" placeholder="Entrer l'adresse email" name="mail" required>
 
                 <label><b>Confirmation Adresse Email</b></label>
-                <input type="text" id="mail2" placeholder="Entrer l'adresse email" name="text" required>
+                <input type="password" id="mail2" placeholder="Entrer l'adresse email" name="mail2" required>
+
 
                 <label><b>Mot de passe</b></label>
-                <input type="password" id="mdp" placeholder="Entrer le mot de passe" name="password" required>
+                <input type="password" id="mdp" placeholder="Entrer le mot de passe" name="mdp" required>
 
                 <label><b>Confirmation Mot de passe</b></label>
-                <input type="password" id="mdp2" laceholder="Entrer le mot de passe" name="password" required>
+                <input type="password" id="mdp2" laceholder="Entrer le mot de passe" name="mdp2" required>
 
                 <input type="submit" id='submit' value='LOGIN' >
                 <?php

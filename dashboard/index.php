fa3f98c815f0c1b0739abc7cce378109b0f286e3
Nicolas Paillard <paillard.nicolas.pro@gmail.com>
2021-10-08 11:37:13 +0200

    ok

--- dashboard/index.php
+++ dashboard/index.php
@@ -120,39 +120,39 @@
             <div class=tile style="background-color: #10893E;"><h1>Work In Progress</h1></div>
             <div class=tile style="background-color: #FF8C00;"><h1>Work In Progress</h1></div>
             <div id=profile class=tile style="background-color: #E74856;"><!-- Module de profil d'utilisateur -->
-            <?php
-              //fix posts names in js innerhtml
-              if(isset($_POST['newnom']) AND !empty($_POST['newnom']) AND $_POST['newnom'] != $user['pseudo']) {
-                $newnom = htmlspecialchars($_POST['newnom']);
-                $insertpseudo = $bdd->prepare("UPDATE membres SET nom = ? WHERE id = ?");
-                $insertpseudo->execute(array($newnom, $_SESSION['id']));
-                header('Location: dashboard?id='.$_SESSION['id']);
-              }
-              if(isset($_POST['newprenom']) AND !empty($_POST['newprenom']) AND $_POST['newprenom'] != $user['pseudo']) {
-                $newprenom = htmlspecialchars($_POST['newprenom']);
-                $insertpseudo = $bdd->prepare("UPDATE membres SET nom = ? WHERE id = ?");
-                $insertpseudo->execute(array($newprenom, $_SESSION['id']));
-                header('Location: dashboard?id='.$_SESSION['id']);
-              }
-              if(isset($_POST['newmail']) AND !empty($_POST['newmail']) AND $_POST['newmail'] != $user['mail']) {
-                $newmail = htmlspecialchars($_POST['newmail']);
-                $insertmail = $bdd->prepare("UPDATE membres SET mail = ? WHERE id = ?");
-                $insertmail->execute(array($newmail, $_SESSION['id']));
-                header('Location: dashboard?id='.$_SESSION['id']);
-              }
-              if(isset($_POST['newmdp1']) AND !empty($_POST['newmdp1']) AND isset($_POST['newmdp2']) AND !empty($_POST['newmdp2'])) {
-                $mdp1 = sha1($_POST['newmdp1']);
-                $mdp2 = sha1($_POST['newmdp2']);
-                if($mdp1 == $mdp2) {
-                $insertmdp = $bdd->prepare("UPDATE membres SET motdepasse = ? WHERE id = ?");
-                $insertmdp->execute(array($mdp1, $_SESSION['id']));
-                header('Location: dashboard?id='.$_SESSION['id']);
-                } else {
-                $msg = "Vos deux mdp ne correspondent pas !";
+              <?php
+                //fix posts names in js innerhtml
+                if(isset($_POST['newnom']) AND !empty($_POST['newnom']) AND $_POST['newnom'] != $user['pseudo']) {
+                  $newnom = htmlspecialchars($_POST['newnom']);
+                  $insertpseudo = $bdd->prepare("UPDATE membres SET nom = ? WHERE id = ?");
+                  $insertpseudo->execute(array($newnom, $_SESSION['id']));
+                  header('Location: dashboard?id='.$_SESSION['id']);
                 }
-              }
-            ?>  
-            <h2>Profil de <?php echo $userinfo['prenom']; ?></h2>
+                if(isset($_POST['newprenom']) AND !empty($_POST['newprenom']) AND $_POST['newprenom'] != $user['pseudo']) {
+                  $newprenom = htmlspecialchars($_POST['newprenom']);
+                  $insertpseudo = $bdd->prepare("UPDATE membres SET nom = ? WHERE id = ?");
+                  $insertpseudo->execute(array($newprenom, $_SESSION['id']));
+                  header('Location: dashboard?id='.$_SESSION['id']);
+                }
+                if(isset($_POST['newmail']) AND !empty($_POST['newmail']) AND $_POST['newmail'] != $user['mail']) {
+                  $newmail = htmlspecialchars($_POST['newmail']);
+                  $insertmail = $bdd->prepare("UPDATE membres SET mail = ? WHERE id = ?");
+                  $insertmail->execute(array($newmail, $_SESSION['id']));
+                  header('Location: dashboard?id='.$_SESSION['id']);
+                }
+                if(isset($_POST['newmdp1']) AND !empty($_POST['newmdp1']) AND isset($_POST['newmdp2']) AND !empty($_POST['newmdp2'])) {
+                  $mdp1 = sha1($_POST['newmdp1']);
+                  $mdp2 = sha1($_POST['newmdp2']);
+                  if($mdp1 == $mdp2) {
+                  $insertmdp = $bdd->prepare("UPDATE membres SET motdepasse = ? WHERE id = ?");
+                  $insertmdp->execute(array($mdp1, $_SESSION['id']));
+                  header('Location: dashboard?id='.$_SESSION['id']);
+                  } else {
+                  $msg = "Vos deux mdp ne correspondent pas !";
+                  }
+                }
+              ?>  
+              <h2>Profil de <?php echo $userinfo['prenom']; ?></h2>
               <label> Nom = <?php echo $userinfo['prenom'];?></label><br/>
               <label>Prénom = <?php echo $userinfo['nom']; ?></label><br/>
               <label>Mail = <?php echo $userinfo['mail']; ?></label><br/>
